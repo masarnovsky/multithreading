16af38959847accebb1ddb512517fd45d73baffc
m.masarnovsky <m.masarnovsky@gmail.com>
2021-11-28 14:19:07 +0300

    update for atomic

--- src/main/java/by/masarnovsky/multithreading/atomic/Test.java
+++ src/main/java/by/masarnovsky/multithreading/atomic/Test.java
@@ -1,5 +1,7 @@
 package by.masarnovsky.multithreading.atomic;
 
+import java.nio.ByteBuffer;
+import java.util.Base64;
 import java.util.UUID;
 
 public class Test {
@@ -9,6 +11,7 @@ public class Test {
         state = new AtomicVersionedReference<>(object, getVersion());
     }
 
+    // версия считается независимо
     public void updateState(ComplexObject newState) {
         ComplexObject reference;
         String version;
@@ -18,7 +21,30 @@ public class Test {
         } while(state.compareAndSet(reference, newState, version, getVersion()));
     }
 
+    // версия считается независимо
     public String getVersion() {
         return UUID.randomUUID().toString();
+    }
+
+    // версия рассчитывается на основе предыдущей
+    public void updateState1(ComplexObject newState) {
+        ComplexObject reference;
+        String version;
+        String newVersion;
+
+        do {
+            reference = state.getReference();
+            version = state.getVersion();
+            newVersion = getVersion1(version);
+        } while (state.compareAndSet(reference, newState, version, newVersion));
+    }
+
+    // версия рассчитывается на основе предыдущей
+    public String getVersion1(String version) {
+        byte[] decoded = Base64.getDecoder().decode(version);
+        Integer decodedInt = ByteBuffer.wrap(decoded).getInt();
+        Integer nextVersion = decodedInt + 1;
+        byte[] encodeBase = ByteBuffer.allocate(4).putInt(nextVersion).array();
+        return Base64.getEncoder().encodeToString(encodeBase);
     }
 }
