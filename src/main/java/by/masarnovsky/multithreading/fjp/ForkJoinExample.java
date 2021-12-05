d6cb415e2fb6640d77a78a1a85b942cfe02e98e9
m.masarnovsky <m.masarnovsky@gmail.com>
2021-12-05 22:10:30 +0300

    fjp examples

--- src/main/java/by/masarnovsky/multithreading/fjp/ForkJoinExample.java
+++ src/main/java/by/masarnovsky/multithreading/fjp/ForkJoinExample.java
@@ -1,14 +1,18 @@
 package by.masarnovsky.multithreading.fjp;
 
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executors;
 import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.ForkJoinTask;
 import java.util.stream.IntStream;
 
 public class ForkJoinExample {
     public static void main(String[] args) throws ExecutionException, InterruptedException {
-        ForkJoinPool forkJoinPool = new ForkJoinPool(4);
-        forkJoinPool.submit(() -> System.out.println(
+        ForkJoinPool forkJoinPool = (ForkJoinPool) Executors.newWorkStealingPool(4);
+        ForkJoinTask<?> fjt = forkJoinPool.submit(() -> System.out.println(
                 IntStream.range(0, 10000000).average().getAsDouble()
         ));
+
+        fjt.get();
     }
 }
