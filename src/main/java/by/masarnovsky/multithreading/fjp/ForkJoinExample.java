b140a39d686fb4d59f75d59b7bdce44a01d6a083
m.masarnovsky <m.masarnovsky@gmail.com>
2021-12-09 17:17:15 +0300

    forkjoin. refactoring

--- src/main/java/by/masarnovsky/multithreading/fjp/ForkJoinExample.java
+++ src/main/java/by/masarnovsky/multithreading/fjp/ForkJoinExample.java
@@ -1,18 +1,15 @@
 package by.masarnovsky.multithreading.fjp;
 
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.Executors;
 import java.util.concurrent.ForkJoinPool;
-import java.util.concurrent.ForkJoinTask;
 import java.util.stream.IntStream;
 
 public class ForkJoinExample {
-    public static void main(String[] args) throws ExecutionException, InterruptedException {
-        ForkJoinPool forkJoinPool = (ForkJoinPool) Executors.newWorkStealingPool(4);
-        ForkJoinTask<?> fjt = forkJoinPool.submit(() -> System.out.println(
-                IntStream.range(0, 10000000).average().getAsDouble()
+    public static void main(String[] args) throws InterruptedException {
+        ForkJoinPool forkJoinPool = new ForkJoinPool(4);
+        forkJoinPool.execute(() -> System.out.println(
+                IntStream.range(0,10000000).average().getAsDouble()
         ));
 
-        fjt.get();
+        Thread.sleep(1000);
     }
 }
--- src/main/java/by/masarnovsky/multithreading/fjp/ThreadExecutorExample.java
+++ src/main/java/by/masarnovsky/multithreading/fjp/ThreadExecutorExample.java
@@ -1,16 +1,16 @@
 package by.masarnovsky.multithreading.fjp;
 
-import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.IntStream;
 
 public class ThreadExecutorExample {
     public static void main(String[] args) {
-        ExecutorService executor = Executors.newCachedThreadPool();
+        ThreadPoolExecutor executor = (ThreadPoolExecutor) Executors.newCachedThreadPool();
+        executor.setKeepAliveTime(1, TimeUnit.SECONDS);
         executor.submit(() -> System.out.println(
                 IntStream.range(0,10000000).average().getAsDouble()
         ));
-
-        executor.shutdown();
     }
 }
